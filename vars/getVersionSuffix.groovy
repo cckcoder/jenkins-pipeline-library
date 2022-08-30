42f42e5853ef2651c5de1b21ae1fe1bc2c664d51
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-08-30 18:32:41 +0700

    try to use string compare

--- vars/getVersionSuffix.groovy
+++ vars/getVersionSuffix.groovy
@@ -2,7 +2,7 @@ def call(Map config) {
   node {
     echo "${config}"
 
-    if (!config.isReleaseCandidate) {
+    if ("${config.isReleaseCandidate}" == "false") {
       return config.rcNumber + '+ci.' + env.BUILD_NUMBER
     } else {
       return config.rcNumber
