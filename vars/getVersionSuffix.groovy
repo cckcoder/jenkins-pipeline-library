c59ff34c3ed7e9cf2e11c52307a5a38dab3a5e5c
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-08-30 18:27:06 +0700

    merge conflic

--- vars/getVersionSuffix.groovy
+++ vars/getVersionSuffix.groovy
@@ -2,11 +2,10 @@ def call(Map config) {
   node {
     echo "${config}"
 
-    if (config.isReleaseCandidate) {
-      return config.rcNumber
-    } else {
-      echo "here"
+    if (!config.isReleaseCandidate) {
       return config.rcNumber + '+ci.' + env.BUILD_NUMBER
+    } else {
+      return config.rcNumber
     }
   }
 }
